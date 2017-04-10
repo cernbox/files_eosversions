f4363c7336a21157bc7781d344c586138bca7eb4
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2017-04-10 15:31:34 +0200

    Loose installation requirements

--- download.php
+++ download.php
@@ -15,16 +15,20 @@ if($entry) {
 	header('Content-Type:'.$entry->getMimeType());
 	OCP\Response::setContentDispositionHeader(basename($filename), 'attachment');
 	OCP\Response::disableCaching();
-	OCP\Response::setContentLengthHeader($entry->getSize());
+	//OCP\Response::setContentLengthHeader($entry->getSize());
 
 	@ob_end_clean();
 	$handle = $instanceManager->downloadVersion($username, $filename, $revision);
 	if ($handle) {
 		$chunkSize = 8192; // 8 kB chunks
 		while (!feof($handle)) {
-			echo fread($handle, $chunkSize);
+			$buffer = fread($handle, $chunkSize);
+			echo $buffer;
+			//echo fread($handle, $chunkSize);
+			ob_flush();
 			flush();
 		}
+		fclose($handle);
 	}
 } else {
 	header("HTTP/1.0 404 Not Found");
